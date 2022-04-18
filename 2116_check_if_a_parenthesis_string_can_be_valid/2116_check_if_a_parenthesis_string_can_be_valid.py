5c2d2aeb78f633be21bd992eb21bac00a32e1474
timlo0416 <timlo0416@gmail.com>
2022-04-19 00:55:57 +0800

    20220417 project launch.

--- 2116_check_if_a_parenthesis_string_can_be_valid/2116_check_if_a_parenthesis_string_can_be_valid.py
+++ 2116_check_if_a_parenthesis_string_can_be_valid/2116_check_if_a_parenthesis_string_can_be_valid.py
@@ -21,55 +21,39 @@ class Solution(object):
         if len(s) % 2 != 0 or (s[0] == ")" and locked[0] == "1"):
             return False
 
-        unlocked_brace_cnt = 0
-        locked_open_brace_cnt = 0
-        locked_close_brace_cnt = 0
-        locked_diff = 0
-        for idx, i in enumerate(s):
-            if locked[idx] == "1":
-                if i == "(":
-                    locked_diff += 1
-                else:
-                    locked_diff -= 1
-        print("locked_diff = " + str(locked_diff))
-
-        temp_locked_diff = locked_diff
+        unlocked = 0
+        left = 0
+        right = 0
         for idx, i in enumerate(s):
             if locked[idx] == "0":
-                unlocked_brace_cnt += 1
-                if temp_locked_diff > 0 and locked_open_brace_cnt > 0:
-                    unlocked_brace_cnt -= 1
-                    locked_open_brace_cnt -= 1
-                    temp_locked_diff -= 1
+                unlocked += 1
             else:
-                if i == "(":
-                    locked_open_brace_cnt += 1
-                else:
-                    locked_close_brace_cnt += 1
-                    if locked_open_brace_cnt > 0:
-                        locked_open_brace_cnt -= 1
-                        locked_close_brace_cnt -= 1
-                    elif unlocked_brace_cnt > 0:
-                        unlocked_brace_cnt -= 1
-                        locked_close_brace_cnt -= 1
-                    elif temp_locked_diff < locked_diff:
-                        temp_locked_diff += 1
-                        unlocked_brace_cnt += 1
-                        locked_close_brace_cnt -= 1
+                if i == ")":
+                    if left:
+                        left -= 1
+                    elif unlocked:
+                        unlocked -= 1
                     else:
                         return False
-            print("[STEP " + str(idx) + "]")
-            print("unlocked_brace_cnt = " + str(unlocked_brace_cnt))
-            print("locked_open_brace_cnt = " + str(locked_open_brace_cnt))
-            print("locked_close_brace_cnt = " + str(locked_close_brace_cnt))
-            print("temp_locked_diff = " + str(temp_locked_diff))
+                else:
+                    left += 1
 
-        if locked_open_brace_cnt != 0:
-            return False
-        if locked_close_brace_cnt != 0:
-            return False
-        if unlocked_brace_cnt % 2 != 0:
-            return False
+        unlocked = 0
+        locked = locked[::-1]
+        s = s[::-1]
+        for idx, i in enumerate(s):
+            if locked[idx] == "0":
+                unlocked += 1
+            else:
+                if i == "(":
+                    if right:
+                        right -= 1
+                    elif unlocked:
+                        unlocked -= 1
+                    else:
+                        return False
+                else:
+                    right += 1
         return True
 
     print(canBeValid(s, locked))
